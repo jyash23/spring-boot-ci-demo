95b70707346c5c819a315d1faf290cf73973e471
Yash <jauhariyash23@gmail.com>
2026-03-10 14:02:58 +0530

    update

--- src/main/java/com/hello/helloapp/HelloController.java
+++ src/main/java/com/hello/helloapp/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from CI/CD pipeline!..";
+        return "Hello from CI/CD pipeline!.....";
     }
 }
